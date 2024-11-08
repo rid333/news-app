eb496853c247395b8af099ddb04d6922560fccb6
Rid <alridho2003@gmail.com>
2024-11-08 18:36:41 +0800

    (5) Added headline related

--- components/homepage/hero-section/headline.tsx
+++ components/homepage/hero-section/headline.tsx
@@ -8,55 +8,55 @@ import { Badge } from "@/components/ui/badge";
 import HomeImage from "@/public/home.png";
 
 const Headline = () => {
-  return (
-    <div className="flex justify-center items-center gap-6">
-      <div className="flex flex-col gap-3 w-[300px]">
-        <div
-          className={cn(
-            "font-bold text-3xl hover:text-primary cursor-pointer transition",
-            `${PoynterTextRegular.className}`,
-          )}
-        >
-          What Trump's win means for Ukraine, Middle East and China
+    return (
+        <div className="flex justify-center items-center gap-6">
+            <div className="flex flex-col gap-3 w-[300px]">
+                <div
+                    className={cn(
+                        "font-bold text-3xl hover:text-primary cursor-pointer transition",
+                        `${PoynterTextRegular.className}`,
+                    )}
+                >
+                    What Trump's win means for Ukraine, Middle East and China
+                </div>
+                <div className="flex text-sm">
+                    <div className="flex justify-center items-center gap-x-1.5">
+                        <CalendarClock size={16} /> Satu jam yang lalu
+                    </div>
+                    <Dot />
+                    <div className="flex justify-center items-center gap-x-1.5">
+                        <BookOpen size={16} /> 6 menit
+                    </div>
+                </div>
+                <div className={cn("text-base", `${FuturaRegular.className}`)}>
+                    Donald Trump’s return to the White House is set to reshape US foreign
+                    policy, promising potentially radical shifts on multiple fronts as war
+                    and uncertainty grip parts of the world...
+                </div>
+                <div className="flex gap-2 flex-wrap">
+                    <Badge className="cursor-pointer" variant={"default"}>
+                        Politik
+                    </Badge>
+                    <Badge className="cursor-pointer" variant={"default"}>
+                        Bisnis
+                    </Badge>
+                    <Badge className="cursor-pointer" variant={"default"}>
+                        Internasional
+                    </Badge>
+                </div>
+            </div>
+            <div className="w-[500px] overflow-hidden rounded-md">
+                <AspectRatio ratio={5 / 4}>
+                    <Image
+                        fill={true}
+                        src={HomeImage}
+                        alt="Image"
+                        className="rounded-md object-cover hover:scale-110 transition-all duration-500 cursor-pointer"
+                    />
+                </AspectRatio>
+            </div>
         </div>
-        <div className="flex text-sm">
-          <div className="flex justify-center items-center gap-x-1.5">
-            <CalendarClock size={16} /> Satu jam yang lalu
-          </div>
-          <Dot />
-          <div className="flex justify-center items-center gap-x-1.5">
-            <BookOpen size={16} /> 6 menit
-          </div>
-        </div>
-        <div className={cn("text-base", `${FuturaRegular.className}`)}>
-          Donald Trump’s return to the White House is set to reshape US foreign
-          policy, promising potentially radical shifts on multiple fronts as war
-          and uncertainty grip parts of the world...
-        </div>
-        <div className="flex gap-2 flex-wrap">
-          <Badge className="cursor-pointer" variant={"default"}>
-            Politik
-          </Badge>
-          <Badge className="cursor-pointer" variant={"default"}>
-            Bisnis
-          </Badge>
-          <Badge className="cursor-pointer" variant={"default"}>
-            Internasional
-          </Badge>
-        </div>
-      </div>
-      <div className="w-[600px] overflow-hidden rounded-md">
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            fill={true}
-            src={HomeImage}
-            alt="Image"
-            className="rounded-md object-cover hover:scale-110 transition-all duration-500 cursor-pointer"
-          />
-        </AspectRatio>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Headline;
--- components/homepage/hero-section/hero.tsx
+++ components/homepage/hero-section/hero.tsx
@@ -2,12 +2,12 @@ import HeadlineRelated from "@/components/homepage/hero-section/headline-related
 import Headline from "@/components/homepage/hero-section/headline";
 
 const Hero = () => {
-  return (
-    <div className="flex gap-6">
-      <Headline />
-      <HeadlineRelated />
-    </div>
-  );
+    return (
+        <div className="flex gap-6">
+            <Headline />
+            <HeadlineRelated />
+        </div>
+    );
 };
 
 export default Hero;
